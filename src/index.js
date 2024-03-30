7a8bf579e58f7294b1a7d529af4707099aae7781
Aashika Jain <jainaashika1510@gmail.com>
2024-03-30 16:00:55 +0530

    files added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './views/Home/Home';
+import Home from './views/Home/Home.js';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 const root = ReactDOM.createRoot(document.getElementById('root'));
